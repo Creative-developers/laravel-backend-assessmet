cd43901b48f6e56237f4b1d8ff4607c8bc36f8eb
Ahmed Nasrullah <ahmed@adwora.com>
2025-02-20 19:49:44 +0400

    updated route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
          <div class="mt-5">
             <p style="font-weight: bold">Please check Readme.md file provided which include all the details including Swagger API doc and postman json file</p>
             <p>GitHub Repo: <a href="https://github.com/Creative-developers/laravel-backend-assessmet/" target="_blank">https://github.com/Creative-developers/laravel-backend-assessmet/</a></p>
+            <p>Swagger API Documentation: <a href="/api/documentation" target="_blank">/api/documentation</a></p>
          </div>
     </body>
 </html>
